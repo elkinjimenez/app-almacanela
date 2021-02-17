5c5eb37850ef77563ebab12ed909681b165826dd
Elkin Omar Jiménez García <elomjiga@hotmail.com>
2021-02-17 16:31:54 -0500

    Ejemplos iniciales de datos de sección de inicio.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './Principal/contacto/contacto.component';
 import { LandingPageComponent } from './Principal/landing-page/landing-page.component';
+import { QuienesSomosComponent } from './Principal/quienes-somos/quienes-somos.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'quienes-somos', component: QuienesSomosComponent },
 ];
 
 @NgModule({
